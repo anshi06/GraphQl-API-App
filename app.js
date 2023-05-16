e98e2f131b2925eae323da9fe68f814a8511d646
anshi06 <aradhya6768@gmail.com>
2023-05-16 14:49:19 +0530

    Authentication-Added middleware

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const { graphqlHTTP } = require("express-graphql");
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 const mongoose = require("mongoose");
+const isAuth = require('./middleware/is-auth');
 
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(isAuth);
 app.use(
   "/graphql",
   graphqlHTTP({
